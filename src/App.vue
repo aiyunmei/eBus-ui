a0a09417910da7b37b34ea0e03d18dda01d6948a
zc <z304307393@163.com>
2018-01-09 09:50:35 +0800

    xxx

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
   export default {
     name: 'app',
     mounted () {
-      sessionStorage.setItem('userId', '2088302427608644')
+      // sessionStorage.setItem('userId', '2088302427608644')
       // sessionStorage.setItem('userId', '2088402107362849')
       // sessionStorage.setItem('userId', '2088302733676494')
       // sessionStorage.setItem('userId', '2088002137597934')
